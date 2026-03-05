a78272ad2e49b8cc91216a07d3387f26e1f9574f
ironhamlab <03110315@naver.com>
2026-03-05 23:52:51 +0900

    refactor : apps 디렉토리 생성 및 구조 수정

--- config/settings.py
+++ config/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'apps.accounts',
+    'apps.whisky',
+    'apps.tasting',
+    'apps.collection',
+    'apps.curator',
 ]
 
 MIDDLEWARE = [
